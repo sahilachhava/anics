a3fede488a59d7e5f79dd41a8614e63cc2fb0b51
Mohammad Sahil Achhava <sahilachhava07@gmail.com>
2021-07-17 18:59:00 +0530

    admin panel changes
    added trending section form for dynamic handling of home page trending section also some bug fixes

--- js/home.js
+++ js/home.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {  
     setTimeout(()=>{
+        document.getElementById("hero-video").load();
         document.getElementById("hero-video").play();
     }, 5000);
 
-    //onscroll change header color 
+    // onscroll change header color 
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 100) {
-            $("header").css("background-color", "black");
-            document.getElementById("hero-video").pause();
-        } else if($(window).scrollTop() < 10) {
-            $("header").css("background-color", "transparent");
-            document.getElementById("hero-video").play();
-        }
+        if($("#hero-video").prop('currentTime') > 0){
+            if($(window).scrollTop() > 100) {
+                $("header").css("background-color", "black");
+                document.getElementById("hero-video").pause();
+            } else if($(window).scrollTop() < 10) {
+                $("header").css("background-color", "transparent");
+                document.getElementById("hero-video").play();
+            }
+        } 
     });
 });
 
